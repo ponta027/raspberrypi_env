9663392854e58a1b251438104c744eb73535a229
ponta027 <ponta027@gmail.com>
2021-02-09 20:40:47 +0900

    add get package command

--- golang/install.sh
+++ golang/install.sh
@@ -2,5 +2,11 @@ wget https://golang.org/dl/go1.15.8.linux-armv6l.tar.gz
 sudo tar -C /usr/local/ -zxvf go1.15.8.linux-armv6l.tar.gz
 cat /usr/local/go/VERSION
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+echo 'GOPATH=$(go env GOPATH)' >> ~/.bashrc 
+echo 'export PATH=${PATH}:${GOPATH}/bin' >> ~/.bashrc
 
+source ~/.bashrc
+
+go get github.com/motemen/gore/cmd/gore
+go get golang.org/x/tools/cmd/goimports
