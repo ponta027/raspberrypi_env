f08284336a05dfebc4cf574eb043000b2256f73d
ponta027 <ponta027@gmail.com>
2021-02-09 18:45:57 +0900

    add docker

---
+++ fluentd/run.sh
@@ -0,0 +1,2 @@
+docker run -d --name=fluentd -p 24224:24224 -v /home/pi/logs:/fluentd/logs fluentd
+
---
+++ install_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -sSL https://get.docker.com | sh
+sudo pip3 install docker-compose
