4dfbd03be6fa38b841babb2793038f0796364bff
ponta027 <ponta027@gmail.com>
2021-04-24 18:54:07 +0900

    delete space

--- samba/mount_hdd.sh
+++ samba/mount_hdd.sh
@@ -4,4 +4,4 @@ sudo apt-get install cifs-utils
 read -sp "user:" user
 read -sp "Password:" pass
 read -sp "samba_server:" server
-sudo mount -t cifs //${server}/disk1_pt1 /mnt/samba/ -o vers=1.0,user=${user} ,password=${pass}
+sudo mount -t cifs //${server}/disk1_pt1 /mnt/samba -o vers=1.0,user=${user},password=${pass}
