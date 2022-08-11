7a26c9d3d41b5a37cdcd7d2195da9449ff7cb3ea
kazuhiro shibuya <ponta027@gmail.com>
2022-08-11 18:57:41 +0900

    add doxygen env

---
+++ doxygen/data/Readme.md
@@ -0,0 +1,4 @@
+Test 
+=============
+
+sample Readme.
---
+++ doxygen/data/sample.c
@@ -0,0 +1,28 @@
+/**
+ * @file   sample.c
+ * @brief  sample program
+ * @author XXX
+ * @date   YYYY-mm-dd
+ * @startuml
+ * NodeA-NodeB:kazuhiro shibuya/////// 引数の個数
+ * @enduml
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *****************************************************************************
+ * @brief  Sample main program
+ * @param  argc 引数の個数
+ * @param  argv 引数文字列
+ * @return 終了コード
+ * @details
+ *****************************************************************************
+ */
+int main(int argc, char *argv[])
+{
+  printf("Hello World\n");
+  return 0;
+}
+
